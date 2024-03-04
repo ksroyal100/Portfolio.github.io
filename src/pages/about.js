246ae51fbaddc8e0e5f8b92b3f92862db101a665
kishan <guptakishan492@gmail.com>
2024-03-05 01:06:27 +0530

    minor bug

--- src/pages/about.js
+++ src/pages/about.js
@@ -43,14 +43,14 @@ const about = () => {
                   <div className='grid w-full grid-cols-8 gap-16'>
                       <div className='col-span-3 flex flex-col items-start justify-start'>
                           <h2 className='mb-4 text-lg font-bold uppercase text-black-300'>Biography</h2>
-                          <p className='font-medium'> Hi, I'm CodeBucks, a web developer and UI/UX designer with a passion for creating beautiful, functional, 
+                          <p className='font-medium'> Hi, I am CodeBucks, a web developer and UI/UX designer with a passion for creating beautiful, functional, 
 and user-centered digital experiences. With 4 years of experience in the field. I am always looking for 
-new and innovative ways to bring my clients' visions to life. </p>
-<p className='my-4 font-medium'> I believe that design is about more than just making things look pretty it's about solving problems and 
+new and innovative ways to bring my clients visions to life. </p>
+<p className='my-4 font-medium'> I believe that design is about more than just making things look pretty it is about solving problems and 
 creating intuitive, enjoyable experiences for users. 
 </p>
 
-<p className='font-medium'> Whether I'm working on a website, mobile app, or 
+<p className='font-medium'> Whether I am working on a website, mobile app, or 
 other digital product, I bring my commitment to design excellence and user-centered thinking to 
 every project I work on. I look forward to the opportunity to bring my skills and passion to your next project.</p>
                       </div>
